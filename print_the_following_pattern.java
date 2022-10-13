15ead2478c002a7ccbb75a55b8abcb886323528e
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-13 17:15:58 +0530

    import java.util.Scanner; class P{     public static void main(String args[]){        Scanner x=new Scanner(System.in) ;        int n=x.nextInt();        for(int i=n;i>=1;i--){            for(int j=1;j<=i;j++){                System.out.print(j+"");
               }System.out.println();
           }
        }
    }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class P{
+    public static void main(String args[]){
+       Scanner x=new Scanner(System.in) ;
+       int n=x.nextInt();
+       for(int i=n;i>=1;i--){
+           for(int j=1;j<=i;j++){
+               System.out.print(j+"");
+        
+           }System.out.println();
+       }
+    }
+}
