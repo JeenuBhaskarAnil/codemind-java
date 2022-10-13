3ccac5fc41371dcad58e0e3a3ecaa6ed9b32db5f
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-13 17:22:07 +0530

    import java.util.Scanner; class P{     public static void main(String args[]){        Scanner x=new Scanner(System.in) ;        int n=x.nextInt();        for(int i=1;i<=n;i++){            for(int j=1;j<=n;j++){                System.out.print(i+" ");
               }System.out.println();
           }
        }
    }

---
+++ Pattern.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class P{
+    public static void main(String args[]){
+       Scanner x=new Scanner(System.in) ;
+       int n=x.nextInt();
+       for(int i=1;i<=n;i++){
+           for(int j=1;j<=n;j++){
+               System.out.print(i+" ");
+        
+           }System.out.println();
+       }
+    }
+}
