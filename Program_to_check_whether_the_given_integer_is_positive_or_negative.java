1f0ea876382acf9f3215d46ef939764b3d97084c
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-14 22:40:05 +0530

    import java.util.Scanner; import java.lang.Math; class A{     public static void main(String args[]){         int a,b,c;         double s=0.00, ar=0.0;         Scanner x = new Scanner(System.in);         a= x.nextInt();         if (a<0){         System.out.print("Negative Number");         }else{             System.out.print("Positive Number");         }     } }

---
+++ Program_to_check_whether_the_given_integer_is_positive_or_negative.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.lang.Math;
+class A{
+    public static void main(String args[]){
+        int a,b,c;
+        double s=0.00, ar=0.0;
+        Scanner x = new Scanner(System.in);
+        a= x.nextInt();
+        if (a<0){
+        System.out.print("Negative Number");
+        }else{
+            System.out.print("Positive Number");
+        }
+    }
+}
