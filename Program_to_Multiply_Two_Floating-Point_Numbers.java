12d63f3c4e7b482e6c2e92558e79f4ecd6883f4a
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-14 22:17:31 +0530

    import java.util.Scanner; import java.lang.Math; class A{     public static void main(String args[]){         double a,b;         Scanner x = new Scanner(System.in);         a= x.nextDouble();         b=x.nextDouble();         System.out.format("%.2f",a*b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.lang.Math;
+class A{
+    public static void main(String args[]){
+        double a,b;
+        Scanner x = new Scanner(System.in);
+        a= x.nextDouble();
+        b=x.nextDouble();
+        System.out.format("%.2f",a*b);
+    }
+}
