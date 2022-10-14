65e24a1c04a12154c008b29bdde151c7cda5cf2d
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-14 23:51:42 +0530

    import java.util.Scanner; import java.lang.Math; class A{     public static void main(String args[]){         Scanner x = new Scanner(System.in);         int a=x.nextInt();         int b=x.nextInt();         int c=x.nextInt();        int f=(a*b*c)/100;         System.out.print(f);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+import java.lang.Math;
+class A{
+    public static void main(String args[]){
+        Scanner x = new Scanner(System.in);
+        int a=x.nextInt();
+        int b=x.nextInt();
+        int c=x.nextInt();
+       int f=(a*b*c)/100;
+        System.out.print(f);
+        
+    }
+}
