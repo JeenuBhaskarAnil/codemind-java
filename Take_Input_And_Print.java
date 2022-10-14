e4d67a6173836488eb861070ddc4336e3939073f
JeenuBhaskarAnil <99954362+JeenuBhaskarAnil@users.noreply.github.com>
2022-10-14 22:23:29 +0530

    import java.util.Scanner; import java.lang.Math; class A{     public static void main(String args[]){         String s;         Scanner x = new Scanner(System.in);         s= x.nextLine();         System.out.format(s);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.lang.Math;
+class A{
+    public static void main(String args[]){
+        String s;
+        Scanner x = new Scanner(System.in);
+        s= x.nextLine();
+        System.out.format(s);
+    }
+}
